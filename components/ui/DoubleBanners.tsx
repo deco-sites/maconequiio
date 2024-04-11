56a9c6ea200ae395da59d058fb9078e81547d06b
@yuri_assuncx <killerkoll2012@gmail.com>
2024-04-11 19:29:40 -0300

    feat: DoubleBanners section created

---
+++ components/ui/DoubleBanners.tsx
@@ -0,0 +1,77 @@
+import Image from "apps/website/components/Image.tsx";
+import type { HTMLWidget, ImageWidget } from "apps/admin/widgets.ts";
+
+/**
+ * @altBy title
+ */
+export interface Banner {
+  image: ImageWidget;
+  alt: string;
+  title: string;
+  description: HTMLWidget;
+  /**
+   * @default #
+   */
+  link: string;
+  target?: "_blank" | "_self";
+}
+
+export interface Props {
+  title?: HTMLWidget;
+  /**
+   * @minItems 2
+   * @maxItems 2
+   */
+  banners?: Banner[];
+}
+
+function BannerCard(
+  { image, alt, title, description, link = "#", target = "_blank" }: Banner,
+) {
+  return (
+    <div class="flex flex-col items-center justify-center gap-6 w-full h-full">
+      <a
+        href={link}
+        target={target}
+      >
+        <Image
+          src={image}
+          width={642}
+          height={414}
+          alt={alt}
+          loading="lazy"
+          fetchPriority="low"
+        />
+      </a>
+
+      <div class="flex flex-col gap-2 items-center justify-center">
+        <h2 class="leading-5 font-bold text-center">{title}</h2>
+        {description && (
+          <div
+            class="text-center leading-4 text-sm"
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default function DoubleBanners({ title, banners = [] }: Props) {
+  if (!banners || banners.length === 0) return null;
+
+  return (
+    <div class="w-full container py-8 flex flex-col gap-10 lg:py-10 max-w-[95%] xl:max-w-[80%] 2xl:max-w-[1350px]">
+      {title && (
+        <div
+          class="leading-[18px] lg:text-[26px] lg:leading-[30px] text-nowrap"
+          dangerouslySetInnerHTML={{ __html: title }}
+        />
+      )}
+
+      <div class="grid lg:grid-cols-2 gap-4 w-full">
+        {banners.map((banner) => <BannerCard {...banner} />)}
+      </div>
+    </div>
+  );
+}
--- manifest.gen.ts
+++ manifest.gen.ts
@@ -35,31 +35,32 @@ import * as $$$$$$27 from "./sections/Header/Header.tsx";
 import * as $$$$$$28 from "./sections/Images/BannerGrid.tsx";
 import * as $$$$$$29 from "./sections/Images/Carousel.tsx";
 import * as $$$$$$30 from "./sections/Images/CarouselBannerWithTitle.tsx";
-import * as $$$$$$31 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$32 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$33 from "./sections/Layout/Container.tsx";
-import * as $$$$$$34 from "./sections/Layout/Flex.tsx";
-import * as $$$$$$35 from "./sections/Layout/Grid.tsx";
-import * as $$$$$$36 from "./sections/Layout/GridItem.tsx";
-import * as $$$$$$37 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$38 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$39 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$40 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$41 from "./sections/Miscellaneous/Slide.tsx";
-import * as $$$$$$42 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$43 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$44 from "./sections/Product/ImageGallerySlider.tsx";
-import * as $$$$$$45 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$46 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$47 from "./sections/Product/ProductInfo.tsx";
-import * as $$$$$$48 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$49 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$50 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$51 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$52 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$53 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$54 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$55 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$31 from "./sections/Images/DoubleBanners.tsx";
+import * as $$$$$$32 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$33 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$34 from "./sections/Layout/Container.tsx";
+import * as $$$$$$35 from "./sections/Layout/Flex.tsx";
+import * as $$$$$$36 from "./sections/Layout/Grid.tsx";
+import * as $$$$$$37 from "./sections/Layout/GridItem.tsx";
+import * as $$$$$$38 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$39 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$40 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$41 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$42 from "./sections/Miscellaneous/Slide.tsx";
+import * as $$$$$$43 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$44 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$45 from "./sections/Product/ImageGallerySlider.tsx";
+import * as $$$$$$46 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$47 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$48 from "./sections/Product/ProductInfo.tsx";
+import * as $$$$$$49 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$50 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$51 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$52 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$53 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$54 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$55 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$56 from "./sections/Theme/Theme.tsx";
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 
@@ -103,32 +104,33 @@ const manifest = {
     "deco-sites/maconequiio/sections/Images/Carousel.tsx": $$$$$$29,
     "deco-sites/maconequiio/sections/Images/CarouselBannerWithTitle.tsx":
       $$$$$$30,
-    "deco-sites/maconequiio/sections/Images/ImageGallery.tsx": $$$$$$31,
-    "deco-sites/maconequiio/sections/Images/ShoppableBanner.tsx": $$$$$$32,
-    "deco-sites/maconequiio/sections/Layout/Container.tsx": $$$$$$33,
-    "deco-sites/maconequiio/sections/Layout/Flex.tsx": $$$$$$34,
-    "deco-sites/maconequiio/sections/Layout/Grid.tsx": $$$$$$35,
-    "deco-sites/maconequiio/sections/Layout/GridItem.tsx": $$$$$$36,
-    "deco-sites/maconequiio/sections/Links/LinkTree.tsx": $$$$$$37,
-    "deco-sites/maconequiio/sections/Links/Shortcuts.tsx": $$$$$$38,
-    "deco-sites/maconequiio/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$39,
-    "deco-sites/maconequiio/sections/Miscellaneous/CookieConsent.tsx": $$$$$$40,
-    "deco-sites/maconequiio/sections/Miscellaneous/Slide.tsx": $$$$$$41,
-    "deco-sites/maconequiio/sections/Newsletter/Newsletter.tsx": $$$$$$42,
+    "deco-sites/maconequiio/sections/Images/DoubleBanners.tsx": $$$$$$31,
+    "deco-sites/maconequiio/sections/Images/ImageGallery.tsx": $$$$$$32,
+    "deco-sites/maconequiio/sections/Images/ShoppableBanner.tsx": $$$$$$33,
+    "deco-sites/maconequiio/sections/Layout/Container.tsx": $$$$$$34,
+    "deco-sites/maconequiio/sections/Layout/Flex.tsx": $$$$$$35,
+    "deco-sites/maconequiio/sections/Layout/Grid.tsx": $$$$$$36,
+    "deco-sites/maconequiio/sections/Layout/GridItem.tsx": $$$$$$37,
+    "deco-sites/maconequiio/sections/Links/LinkTree.tsx": $$$$$$38,
+    "deco-sites/maconequiio/sections/Links/Shortcuts.tsx": $$$$$$39,
+    "deco-sites/maconequiio/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$40,
+    "deco-sites/maconequiio/sections/Miscellaneous/CookieConsent.tsx": $$$$$$41,
+    "deco-sites/maconequiio/sections/Miscellaneous/Slide.tsx": $$$$$$42,
+    "deco-sites/maconequiio/sections/Newsletter/Newsletter.tsx": $$$$$$43,
     "deco-sites/maconequiio/sections/Product/ImageGalleryFrontBack.tsx":
-      $$$$$$43,
-    "deco-sites/maconequiio/sections/Product/ImageGallerySlider.tsx": $$$$$$44,
-    "deco-sites/maconequiio/sections/Product/NotFound.tsx": $$$$$$45,
-    "deco-sites/maconequiio/sections/Product/NotFoundChallenge.tsx": $$$$$$46,
-    "deco-sites/maconequiio/sections/Product/ProductInfo.tsx": $$$$$$47,
-    "deco-sites/maconequiio/sections/Product/ProductShelf.tsx": $$$$$$48,
-    "deco-sites/maconequiio/sections/Product/ProductShelfTabbed.tsx": $$$$$$49,
-    "deco-sites/maconequiio/sections/Product/SearchResult.tsx": $$$$$$50,
-    "deco-sites/maconequiio/sections/Product/ShelfWithImage.tsx": $$$$$$51,
-    "deco-sites/maconequiio/sections/Product/Wishlist.tsx": $$$$$$52,
-    "deco-sites/maconequiio/sections/Social/InstagramPosts.tsx": $$$$$$53,
-    "deco-sites/maconequiio/sections/Social/WhatsApp.tsx": $$$$$$54,
-    "deco-sites/maconequiio/sections/Theme/Theme.tsx": $$$$$$55,
+      $$$$$$44,
+    "deco-sites/maconequiio/sections/Product/ImageGallerySlider.tsx": $$$$$$45,
+    "deco-sites/maconequiio/sections/Product/NotFound.tsx": $$$$$$46,
+    "deco-sites/maconequiio/sections/Product/NotFoundChallenge.tsx": $$$$$$47,
+    "deco-sites/maconequiio/sections/Product/ProductInfo.tsx": $$$$$$48,
+    "deco-sites/maconequiio/sections/Product/ProductShelf.tsx": $$$$$$49,
+    "deco-sites/maconequiio/sections/Product/ProductShelfTabbed.tsx": $$$$$$50,
+    "deco-sites/maconequiio/sections/Product/SearchResult.tsx": $$$$$$51,
+    "deco-sites/maconequiio/sections/Product/ShelfWithImage.tsx": $$$$$$52,
+    "deco-sites/maconequiio/sections/Product/Wishlist.tsx": $$$$$$53,
+    "deco-sites/maconequiio/sections/Social/InstagramPosts.tsx": $$$$$$54,
+    "deco-sites/maconequiio/sections/Social/WhatsApp.tsx": $$$$$$55,
+    "deco-sites/maconequiio/sections/Theme/Theme.tsx": $$$$$$56,
   },
   "apps": {
     "deco-sites/maconequiio/apps/decohub.ts": $$$$$$$$$$$0,
