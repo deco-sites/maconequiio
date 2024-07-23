5492e5d2de8095d70e6f6f2c377aebd0b80a3269
@yuri_assuncx <killerkoll2012@gmail.com>
2024-07-23 13:59:15 -0300

    style: added Button classes to User

--- components/user/User.tsx
+++ components/user/User.tsx
@@ -1,9 +1,12 @@
 import Icon from "deco-sites/maconequiio/components/ui/Icon.tsx";
+import Button from "deco-sites/maconequiio/components/ui/Button.tsx";
 
 export default function User() {
   return (
     <a href="/account">
-      <Icon id="User" size={20} strokeWidth={0.4} class="text-red" />
+      <Button class="btn-circle btn-sm btn-ghost">
+        <Icon id="User" size={20} strokeWidth={0.4} class="text-red" />
+      </Button>
     </a>
   );
 }
