243699288476e94eaa927e000359a904f9c92d01
@yuri_assuncx <killerkoll2012@gmail.com>
2024-07-23 14:51:13 -0300

    style (CategoryBanner): padding added to title and subtitle

--- components/ui/CategoryBanner.tsx
+++ components/ui/CategoryBanner.tsx
@@ -66,16 +66,13 @@ function Banner(props: SectionProps<ReturnType<typeof loader>>) {
         <img class="w-full" src={image.desktop} alt={image.alt ?? title} />
       </Picture>
 
-      <div class="container flex flex-col items-center justify-center sm:items-start col-start-1 col-span-1 row-start-1 row-span-1 w-full">
-        <h1>
-          <span class="text-5xl font-medium text-base-100">
-            {title}
-          </span>
+      <div class="container flex flex-col items-center justify-center sm:items-start col-start-1 col-span-1 row-start-1 row-span-1 w-full px-4">
+        <h1 class="w-full text-5xl font-medium text-base-100">
+          {title}
         </h1>
-        <h2>
-          <span class="text-xl font-medium text-base-100">
-            {subtitle}
-          </span>
+
+        <h2 class="w-full text-xl font-medium text-base-100">
+          {subtitle}
         </h2>
       </div>
     </div>
